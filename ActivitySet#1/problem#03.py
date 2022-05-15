3549a830c9a987615707a80f467b454ec545e381
Kushi Kote <97019587+Kushikote@users.noreply.github.com>
2022-05-15 07:30:33 +0000

    .

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,4 @@
-# Variables, Expressions & Statements
-
-hrs = float(input("Enter hours? "))
+hrs = input("Enter hours : ")
+rph = input("Enter rate : ")
+gross = float(hrs) * float(rph)
+print("Pay :",gross)
--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,31 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+h = float(input("Enter Hours:"))
+x = float(input("Enter the Rate:"))
+if h <= 40:
+ 	print( h  * x)
+elif h > 40:
+	print(40* x + (h-40)*1.5*x)
+
+# ActivitySet#1/problem#04.py
+
+
+
+'''score = input("Enter Score: ")
+s = float(score)
+if s<=1.0 and s>=0.0:
+    if s>=0.9:
+      print("A")
+    elif s>=0.8:
+      print("B")
+    elif s>=0.7:
+      print("C")
+    elif s>=0.6:
+      print("D")
+    elif s<0.6:
+      print("F")
+    elif print("invalid output"):   '''  
+      
+
+
+
