6604d2f0036244e5b20609679a7c24cc6d0b596f
Kushi Kote <97019587+Kushikote@users.noreply.github.com>
2022-05-25 16:30:33 +0000

    10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -4,7 +4,7 @@ filename = "dataset/mbox-short.txt"
 
 
 
-'''name = input("Enter file name: ")
+name = input("Enter file name: ")
 fh = open(name)
 words=list()
 
@@ -17,8 +17,28 @@ for line in fh :
         else:
             counts[words[1]]+=1
 
-wo=counts.values()
-z=max(wo)
+you=counts.values()
+me=max(you)
 for key in counts:
     if counts[key]==z:
-      print(key,z)'''
+      print(key,z)
+
+
+''''name = input("Enter file:")
+fname=open(name)
+word=[]
+
+counts=dict()
+for lines in fname:
+    if lines.startswith("From:"):
+        word=lines.split()
+    if word[1] in counts:
+        counts[word[1]]+=1
+    else:
+        counts[word[1]]=1
+        
+me=counts.value()
+you=max(me)
+for key in counts:
+    if counts(key)==you:
+        print(key,you)''''
