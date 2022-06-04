dafcbd13a6d06c6edf8420802ef03d3f1e9041f8
Kushi Kote <97019587+Kushikote@users.noreply.github.com>
2022-06-04 16:54:56 +0000

    2

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -2,43 +2,40 @@
 
 filename = "dataset/mbox-short.txt"
 
-
-
 name = input("Enter file name: ")
 fh = open(name)
-words=list()
+words = list()
 
 counts = dict()
-for line in fh :
-    if line.startswith("From:") :
-        words = line.split()
-        if words[1] not in counts:
-            counts[words[1]]=1
-        else:
-            counts[words[1]]+=1
+for line in fh:
+	if line.startswith("From:"):
+		words = line.split()
+		if words[1] not in counts:
+			counts[words[1]] = 1
+		else:
+			counts[words[1]] += 1
 
-you=counts.values()
-me=max(you)
+you = counts.values()
+me = max(you)
 for key in counts:
-    if counts[key]==z:
-      print(key,z)
+	if counts[key] == z:
+		print(key, z)
 
+name = input("Enter file:")
+fname = open(name)
+word = []
 
-''''name = input("Enter file:")
-fname=open(name)
-word=[]
-
-counts=dict()
+counts = dict()
 for lines in fname:
-    if lines.startswith("From:"):
-        word=lines.split()
-    if word[1] in counts:
-        counts[word[1]]+=1
-    else:
-        counts[word[1]]=1
-        
-me=counts.value()
-you=max(me)
+	if lines.startswith("From:"):
+		word = lines.split()
+	if word[1] in counts:
+		counts[word[1]] += 1
+	else:
+		counts[word[1]] = 1
+
+me = counts.value()
+you = max(me)
 for key in counts:
-    if counts(key)==you:
-        print(key,you)''''
+	if counts(key) == you:
+		print(key, you)
